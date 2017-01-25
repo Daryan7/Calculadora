9cb38f381c88732e6e1c8c73f90743b24e8820c0
Daryan7 <mario_bcn2@hotmail.es>
2017-01-25 11:12:17 +0100

    Initial commit

---
+++ app/src/main/java/com/example/juan/calculadora/CalculatorActivity.java
@@ -0,0 +1,13 @@
+package com.example.juan.calculadora;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstaceState) {
+        super.onCreate(savedInstaceState);
+        setContentView(R.layout.activity_test);
+    }
+}
