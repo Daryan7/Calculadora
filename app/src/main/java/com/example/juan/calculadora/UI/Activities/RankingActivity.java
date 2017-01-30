c97bd2ac4be1cb08e6df65589d7b887f0bc80081
Juan Miguel <juanmiguel1996@gmail.com>
2017-01-30 21:56:54 +0100

    Implementando fragments

--- app/src/main/java/com/example/juan/calculadora/UI/Activities/RankingActivity.java
+++ app/src/main/java/com/example/juan/calculadora/UI/Activities/RankingActivity.java
@@ -1,18 +1,27 @@
 package com.example.juan.calculadora.UI.Activities;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 
 import com.example.juan.calculadora.Data.DBHelper;
 import com.example.juan.calculadora.Data.User;
 import com.example.juan.calculadora.R;
 import com.example.juan.calculadora.UI.Adapters.RankingAdapter;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
 
 public class RankingActivity extends Fragment {
     private DBHelper dbHelper;
@@ -23,10 +32,32 @@ public class RankingActivity extends Fragment {
         dbHelper = new DBHelper(getContext());
     }
 
+    /*@Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == 0) {
+                Uri selectedImageUri = data.getData();
+            }
+        }
+    }*/
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_ranking, container, false);
+
+        /*Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        getIntent.setType("image/*");
+
+        Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        pickIntent.setType("image/*");
+
+        Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{pickIntent});
+
+        startActivityForResult(chooserIntent, 0);*/
 
         RecyclerView listView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
         listView.setHasFixedSize(true);
