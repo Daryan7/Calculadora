9c71c0fefeb5fbe3ef3a72c9f3f8114f63c6a9f8
Daryan7 <mario_bcn2@hotmail.es>
2017-02-01 10:51:31 +0100

    Implementando memory

--- app/src/main/java/com/example/juan/theapp/UI/Activities/MemoryFragment.java
+++ app/src/main/java/com/example/juan/theapp/UI/Activities/MemoryFragment.java
@@ -2,6 +2,8 @@ package com.example.juan.theapp.UI.Activities;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.SparseArray;
+import android.util.SparseIntArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,9 +15,13 @@ import com.squareup.picasso.Picasso;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class MemoryFragment extends Fragment {
+
+    private SparseIntArray idMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,6 +29,7 @@ public class MemoryFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_memory, container, false);
 
         final int[] resId = {R.drawable.borg, R.drawable.ferengi, R.drawable.klingon, R.drawable.maquis_emblem, R.drawable.reman, R.drawable.starfleet_tuc, R.drawable.ufp_2290c, R.drawable.romulan};
+        final int[] imageId = {R.id._1, R.id._2, R.id._3, R.id._4, R.id._5, R.id._6, R.id._7, R.id._8, R.id._9, R.id._10, R.id._11, R.id._12, R.id._13, R.id._14, R.id._15, R.id._16};
 
         int numFiles = resId.length;
 
@@ -32,77 +39,22 @@ public class MemoryFragment extends Fragment {
             randomArray.add(i % numFiles);
         }
 
+        idMap = new SparseIntArray(numFiles*2);
+
         Random random = new Random(System.currentTimeMillis());
 
-        /*for (int i = 0; i < count; ++i) {
-            ImageView image = (ImageView) gridLayout.getChildAt(i);
+        for (int anImageId : imageId) {
             int randNum = random.nextInt(randomArray.size());
-            //Picasso.with(getContext()).load(resId[randomArray.get(randNum)]).into(image);
-            image.setBackgroundResource(resId[randomArray.get(randNum)]);
+            idMap.put(anImageId, resId[randomArray.get(randNum)]);
+            rootView.findViewById(anImageId).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ImageView image = (ImageView) view;
+                    image.setImageResource(idMap.get(view.getId()));
+                }
+            });
             randomArray.remove(randNum);
-        }*/
-        ImageView image = (ImageView) rootView.findViewById(R.id._1);
-        int randNum = random.nextInt(randomArray.size());
-        image.setImageResource(resId[randomArray.get(randNum)]);
-        randomArray.remove(randNum);
-        image = (ImageView) rootView.findViewById(R.id._2);
-        randNum = random.nextInt(randomArray.size());
-        image.setImageResource(resId[randomArray.get(randNum)]);
-        randomArray.remove(randNum);
-        image = (ImageView) rootView.findViewById(R.id._3);
-        randNum = random.nextInt(randomArray.size());
-        image.setImageResource(resId[randomArray.get(randNum)]);
-        randomArray.remove(randNum);
-        image = (ImageView) rootView.findViewById(R.id._4);
-        randNum = random.nextInt(randomArray.size());
-        image.setImageResource(resId[randomArray.get(randNum)]);
-        randomArray.remove(randNum);
-        image = (ImageView) rootView.findViewById(R.id._5);
-        randNum = random.nextInt(randomArray.size());
-        image.setImageResource(resId[randomArray.get(randNum)]);
-        randomArray.remove(randNum);
-        image = (ImageView) rootView.findViewById(R.id._6);
-        randNum = random.nextInt(randomArray.size());
-        image.setImageResource(resId[randomArray.get(randNum)]);
-        randomArray.remove(randNum);
-        image = (ImageView) rootView.findViewById(R.id._7);
-        randNum = random.nextInt(randomArray.size());
-        image.setImageResource(resId[randomArray.get(randNum)]);
-        randomArray.remove(randNum);
-        image = (ImageView) rootView.findViewById(R.id._8);
-        randNum = random.nextInt(randomArray.size());
-        image.setImageResource(resId[randomArray.get(randNum)]);
-        randomArray.remove(randNum);
-        image = (ImageView) rootView.findViewById(R.id._9);
-        randNum = random.nextInt(randomArray.size());
-        image.setImageResource(resId[randomArray.get(randNum)]);
-        randomArray.remove(randNum);
-        image = (ImageView) rootView.findViewById(R.id._10);
-        randNum = random.nextInt(randomArray.size());
-        image.setImageResource(resId[randomArray.get(randNum)]);
-        randomArray.remove(randNum);
-        image = (ImageView) rootView.findViewById(R.id._11);
-        randNum = random.nextInt(randomArray.size());
-        image.setImageResource(resId[randomArray.get(randNum)]);
-        randomArray.remove(randNum);
-        image = (ImageView) rootView.findViewById(R.id._12);
-        randNum = random.nextInt(randomArray.size());
-        image.setImageResource(resId[randomArray.get(randNum)]);
-        randomArray.remove(randNum);
-        image = (ImageView) rootView.findViewById(R.id._13);
-        randNum = random.nextInt(randomArray.size());
-        image.setImageResource(resId[randomArray.get(randNum)]);
-        randomArray.remove(randNum);
-        image = (ImageView) rootView.findViewById(R.id._14);
-        randNum = random.nextInt(randomArray.size());
-        image.setImageResource(resId[randomArray.get(randNum)]);
-        randomArray.remove(randNum);
-        image = (ImageView) rootView.findViewById(R.id._15);
-        randNum = random.nextInt(randomArray.size());
-        image.setImageResource(resId[randomArray.get(randNum)]);
-        randomArray.remove(randNum);
-        image = (ImageView) rootView.findViewById(R.id._16);
-        image.setImageResource(resId[randomArray.get(0)]);
+        }
 
         return rootView;
     }
